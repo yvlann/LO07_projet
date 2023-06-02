579e59f420c7a0fe23db34d65bb0b1f8f8bcfac4
MiTiX <bast.boulet@gmail.com>
2023-06-02 21:37:47 +0200

    ranking des practiciens /rdv

--- app/view/doctolib/viewUtilisations.php
+++ app/view/doctolib/viewUtilisations.php
@@ -4,36 +4,62 @@ require ($root . '/app/view/fragment/fragmentDoctolibHeader.html');
 ?>
 
 <body style="background-color:#FDEBD0;">
-  <div class="container">
-      <?php
-      require $root . '/app/view/fragment/fragmentDoctolibMenu.php';
-      include $root . '/app/view/fragment/fragmentDoctolibJumbotron.html';
-      ?>
-      <h3>Proposez une fonctionnalité originale</h3>
+    <div class="container">
+        <?php
+        require $root . '/app/view/fragment/fragmentDoctolibMenu.php';
+        include $root . '/app/view/fragment/fragmentDoctolibJumbotron.html';
+        ?>
+        <h3>Nombre de rendez-vous par spécialité et par ville</h3>
       
-    <table class = "table table-striped table-bordered">
-      <thead>
-            <tr>
+        <table class = "table table-striped table-bordered">
+          <thead>
+                <tr>
+                    <?php
+                        $cols = $results_spe[0];
+                        foreach ($cols as $col) {
+                            echo ("<th scope='col'>$col</th>");
+                        }
+                    ?>
+                </tr>
+            </thead>
+          <tbody>
                 <?php
-                    $cols = $results[0];
-                    foreach ($cols as $col) {
-                        echo ("<th scope='col'>$col</th>");
+                    $data = $results_spe[1];             
+                    foreach ($data as $row) {
+                        echo ("<tr>");
+                        foreach ($row as $val) {
+                            echo ("<td>$val</td>");
+                        }
+                        echo ("</tr>");
                     }
                 ?>
-            </tr>
-        </thead>
-      <tbody>
-            <?php
-                $data = $results[1];             
-                foreach ($data as $row) {
-                    echo ("<tr>");
-                    foreach ($row as $val) {
-                        echo ("<td>$val</td>");
+          </tbody>
+        </table>
+        
+        <h3>Praticiens avec le plus de rendez-vous</h3>
+        <table class = "table table-striped table-bordered">
+          <thead>
+                <tr>
+                    <?php
+                        $cols = $results_rank[0];
+                        foreach ($cols as $col) {
+                            echo ("<th scope='col'>$col</th>");
+                        }
+                    ?>
+                </tr>
+            </thead>
+          <tbody>
+                <?php
+                    $data = $results_rank[1];             
+                    foreach ($data as $row) {
+                        echo ("<tr>");
+                        foreach ($row as $val) {
+                            echo ("<td>$val</td>");
+                        }
+                        echo ("</tr>");
                     }
-                    echo ("</tr>");
-                }
-            ?>
-      </tbody>
-    </table>
+                ?>
+          </tbody>
+        </table>
   </div>
   <?php include $root . '/app/view/fragment/fragmentDoctolibFooter.html'; ?>
